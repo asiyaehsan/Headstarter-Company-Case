81a1118c5f1ff84b4733eda74f27590a769002aa
Albert (Haoze Chen) <albertchen2017@outlook.com>
2022-08-18 10:40:15 -0700

    add pages

---
+++ client/src/pages/Signin.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { GoogleButton } from 'react-google-button'
+
+export const Signin = () => {
+  return (
+      <div>
+          Signin
+          <GoogleButton/>
+      </div>
+      
+  )
+}
