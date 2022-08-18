8628b83e3eb4034bbd3fe17ca3c5ab359ff99262
Albert (Haoze Chen) <albertchen2017@outlook.com>
2022-08-18 10:34:26 -0700

    add pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import CalenderPage from "./Components/CalenderPage/CalenderPage";
 
-
 function App() {
   return (
     <div>
+      {/* <NavBar /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />;
+        <Route path="/signin" element={<Signin />} />;
+        <Route path="/account" element={<Account />} />;
+        <Route path="/calendar" element={<CalenderPage />} />;
+      </Routes>
       <CalenderPage />
     </div>
   );
