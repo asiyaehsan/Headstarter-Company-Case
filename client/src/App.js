6fb01af56a19efdd08d359fd69f8f203f2391ca2
Albert (Haoze Chen) <albertchen2017@outlook.com>
2022-08-18 16:42:11 -0700

    finished the authentication

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Signin } from "./pages/Signin";
 import { Account } from "./pages/Account";
+import { Protected } from "./components/Protected";
 import CalenderPage from "./components/CalenderPage";
 import NavBar from "./components/NavBar";
 
@@ -12,12 +13,19 @@ function App() {
     <div>
       <AuthContextProvider>
         <NavBar />
-        {/* <CalenderPage /> */}
         <Routes>
           <Route path="/" element={<Home />} />;
           <Route path="/signin" element={<Signin />} />;
-          <Route path="/account" element={<Account />} />;
-          <Route path="/calendar" element={<CalenderPage />} />;
+          <Route
+            path="/account"
+            element={
+              <Protected>
+                <Account />
+              </Protected>
+            }
+          />
+          ;
+          <Route path="/calender" element={<CalenderPage />} />;
         </Routes>
       </AuthContextProvider>
     </div>
--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,23 +1,32 @@
-import React from 'react'
-import { GoogleButton } from 'react-google-button'
-import { UserAuth } from '../context/AuthContext'
-
-
+import React, { useEffect } from 'react';
+import { GoogleButton } from 'react-google-button';
+import { UserAuth } from '../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
 
 export const Signin = () => {
-    const { googleSignIn } = UserAuth();
-    const handleGoogleSignIn = async () => {
-        try {
-            await googleSignIn();
-        } catch (error) {
-            console.log(error)
-        }
+  const { googleSignIn, user } = UserAuth();
+  const navigate = useNavigate();
+
+  const handleGoogleSignIn = async () => {
+    try {
+      await googleSignIn();
+    } catch (error) {
+      console.log(error);
     }
+  };
+
+  useEffect(() => {
+    if (user != null) {
+      navigate('/account');
+    }
+  }, [user]);
+
   return (
-      <div>
-          Signin
-          <GoogleButton onClick={handleGoogleSignIn} />
+    <div>
+      <h1 className=''>Sign in</h1>
+      <div className=''>
+        <GoogleButton onClick={handleGoogleSignIn} />
       </div>
-      
-  )
-}
+    </div>
+  );
+};
