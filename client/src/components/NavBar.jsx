3370923b1a380014b5c949e714d4bed0dc9b103c
Albert (Haoze Chen) <albertchen2017@outlook.com>
2022-08-20 11:21:47 -0700

    clean up the ui and fix the video player issue

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,35 +1,37 @@
-import React, { useState } from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { UserAuth } from '../context/AuthContext';
+import { Account } from "../pages/Account";
 
-function Navbar() {
-  const [nav, setNav] = useState(false)
-  const handleClick = () => {
-    setNav(!nav)
+const Navbar = () => {
+  const { user, logOut } = UserAuth();
+  const handleSignOut = async () => {
+    try {
+      await logOut()
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return (
     <div className=''>
-      <h1 className=''>
-        Headstarter Group Manager
-      </h1>
       {user?.displayName ? (
         <div className="navBar">
           <Link to='/'>Home</Link>
           <br />
-      {/* <Link to='/account'>Account</Link>
-          <br /> */}
           <Link to='/calendar'>Calendar</Link>
           <br />
           <Link to='/videocall'>Video Call</Link>
-                  <Account/>
         </div>
 
-      ) : (
+      )
+        : (
         <div className="navBar">
-          <Link to='/'>Sign in</Link>
+          {/* <Link to='/'>Sign in</Link> */}
           <br />
-
         </div>
-      )}
+      )
+      }
       {/* {user?.displayName ? (
         <button onClick={handleSignOut}>Logout</button>
       ) : (
@@ -48,3 +50,5 @@ function Navbar() {
     </div>
   );
 };
+
+export default Navbar;
